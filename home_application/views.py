fb9cad59e5ed6964ab5a1b23b5e38b2e9be07e9e
CodeView <46512150+CodeView3@users.noreply.github.com>
2021-11-05 11:44:45 -0700

    Update views.py

--- home_application/views.py
+++ home_application/views.py
@@ -32,7 +32,7 @@ def list(request):
     db.close()
     s=JsonResponse({'names': names})
 
-    return s.encode("utf-8")
+    return s.decode("unicode_escape")
     #JsonResponse({'names': names}).encode("utf-8").decode("unicode_escape")
 
  
